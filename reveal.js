05bac1119caae3ffe5c905b917b3d73ed97eb3b9
Reflyxz <dylanbindaneveseric@gmail.com>
2024-10-09 15:44:57 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,94 @@
+window.revelar = ScrollReveal({reset:true})
+
+// EFEITO 
+revelar.reveal('.eft-logo',
+{
+    duration: 2000,
+    distance: '70px',
+    origin:'top'
+
+})
+
+revelar.reveal('.eft-pesquisa',
+    {
+        duration: 1400,
+        distance: '100px',
+        origin: 'top'
+    
+    })
+
+revelar.reveal('.eft-perfil',
+    {
+        duration: 1100,
+        distance: '100px',
+        origin: 'top',
+        delay: 500
+        
+    })
+
+revelar.reveal('.eft-catalogo',
+     {
+        duration: 1500,
+        distance: '200px',
+        origin: 'left',
+        delay: 200
+            
+     })
+
+     revelar.reveal('.eft-img',
+        {
+           duration: 800,
+           distance: '800px',
+           origin: 'left',
+           delay: 200
+               
+        })   
+
+        revelar.reveal('.eft-frtGratis',
+            {
+               duration: 800,
+               distance: '800px',
+               origin: 'right',
+               delay: 100
+                   
+            })
+
+            revelar.reveal('.eft-txtprod',
+            {
+               duration: 800,
+               distance: '800px',
+               origin: 'left',
+               delay: 100
+                   
+            })
+
+            revelar.reveal('.eft-linha',
+            {
+               duration: 800,
+               distance: '800px',
+               origin: 'right',
+               delay: 100
+                   
+            })
+       
+
+            revelar.reveal('.eft-produtos',
+            {
+               duration: 900,
+               distance: '800px',
+               origin: 'left',
+               delay: 100
+                   
+            })
+
+
+            revelar.reveal('.eft-pesquisa-final',
+            {
+               duration: 900,
+               distance: '800px',
+               origin: 'right',
+               delay: 100
+                   
+            })
+
+            
