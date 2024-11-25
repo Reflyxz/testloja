1cf72fed3fb13fe4614b99596f316bd21bbaffac
Reflyxz <dylanbindaneveseric@gmail.com>
2024-11-25 10:56:52 -0300

    Add files via upload

---
+++ PagesProdutos/app.js
@@ -0,0 +1,22 @@
+var produtoImg = document.getElementById("produtoImg");
+var produtoMiniatura = document.getElementsByClassName("ProdutoMiniatura");
+
+produtoMiniatura[0].onclick = function(){
+    produtoImg.src = ProdutoMiniatura[0].src;
+
+}
+
+produtoMiniatura[1].onclick = function(){
+    produtoImg.src = ProdutoMiniatura[1].src;
+    
+}
+
+produtoMiniatura[2].onclick = function(){
+    produtoImg.src = ProdutoMiniatura[2].src;
+    
+}
+
+produtoMiniatura[3].onclick = function(){
+    produtoImg.src = ProdutoMiniatura[3].src;
+    
+}
